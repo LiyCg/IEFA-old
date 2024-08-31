83f88065dc8c0e921b509fc857c19d04a0f7ac9c
root <leeinyup123@gmail.com>
2024-08-31 03:46:50 +0000

    first

---
+++ src/MEO/actions.py
@@ -0,0 +1,27 @@
+"""
+- holds edited bshp parameter so far
+- activate_blendshape, relative_activate_blendshape
+
+"""
+
+def activate_blendshape(blendshape_target_list, intensity, speed_location, speed,  time):
+    
+    return
+
+
+def relative_activate_blendshape(blendshape_target_list, intensity, speed_location, speed, start_time, end_time):
+    
+    return 
+
+
+"""
+for relative activation, you get current(=self.key_idx) key expression's certain blendshape target parameter value 
+
+"""
+def get_bshp_target_value(self, query_list):
+    
+    for query in query_list:
+        id = full_blendshape_targets.index(query)
+        
+    
+    pass
